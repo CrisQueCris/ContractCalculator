3945e97f35265fc89a230389eb1252b11a10b666
Christoph Rendel <c.rendel@posteo.de>
2022-05-17 13:09:39 +0200

    adsfasdsdf

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output
+from dash import Dash, dcc, html, dash_table
+from dash.dependencies import Input, Output, State
+import pandas as pd
+from datetime import date, timedelta
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+#load table with price data
+future_df = pd.read_csv('Data/future_df.csv')
+
+#load table with contracts
+contracts_df = pd.read_csv('Data/future_df.csv') 
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -16,24 +24,21 @@ app.layout = html.Div([
     
     
     #Tabs that display the different contracts and commodities
-    dcc.Tabs(id="tabs-example-graph", value='tab-1-example-graph', children=[ 
-        dcc.Tab(label='Wheat', value='tab-1-example-graph'),
-        dcc.Tab(label='Barley', value='tab-2-example-graph'),
-        dcc.Tab(label='Corn', value='tab-3-example-graph'),
-        dcc.Tab(label='Rapeseed', value='tab-4-example-graph'),
+    dcc.Tabs(id="commodities-tab", value='wheat_tab_val', children=[ 
+        dcc.Tab(label='wheat', value='wheat-tab-val'),
+        dcc.Tab(label='barley', value='barley-tab-val'),
+        dcc.Tab(label='corn', value='corn-tab-val'),
+        dcc.Tab(label='rapeseed', value='rapeseed-tab-val'),
         ]),
-    html.Div(id='tabs-content-example-graph'),
-                
-            
-            
-            
-            
-            
-            #Graph showing contracted amount
-            
-            
-            #Graph showing commodity price
-            dcc.Graph(
+    html.Div(id='tabs-content', children=[
+    #Dropdown to select the respective Futuresprice
+    dcc.Dropdown(
+    ['Spotprice', 'May 2022', 'June 2022', 'July 2022', 'August 2022', 'September 2022'],
+    ['Spotprice', 'September 2022'],
+    multi=True
+    ),    
+    #Graph showing contracted amount
+    dcc.Graph(
                 figure={
                     'data': [{
                         'x': [1, 2, 3],
@@ -41,33 +46,104 @@ app.layout = html.Div([
                         'type': 'bar'
                     }]
                 }
-            ),
+            ),            
+    #Graph showing commodity price        
     
     
-    dcc.Dropdown(
-    ['Spotprice', 'May 2022', 'June 2022', 'July 2022', 'August 2022', 'September 2022'],
-    ['Spotprice', 'September 2022'],
-    multi=True
-    )    
-    ])
-
-
-
-
-@app.callback(Output('tabs-content-example-graph', "out-all-types",'children'),
-              Input('tabs-example-graph', 'value'))
-def render_content(tab):
-    if tab == 'tab-1-example-graph':
-        return html.Div([
+    
+    #Slider to adapt time
+    dcc.RangeSlider(-5, 10, 1, count=1, value=[-3, 7]),
+    
+    #DatePickerRange to adapt RangSlider
+    dcc.DatePickerRange(
+    id='date-picker-range',
+    start_date=date(1997, 5, 3),
+    end_date_placeholder_text='Select a date!'
+    ),
+    
+    
+    #Table showing contracts
+ 
+    dash_table.DataTable(
+    contracts_df.to_dict('records'),
+    [{"name": i, "id": i} for i in contracts_df.columns]
+    )
+    ]),    
+        
+        
+        
+    #Menu to add a contract
+    html.Div(id = 'contracts-menu',
+        children = [
+            html.Table([
+                html.Caption(title='Enter a new cotract'),
+                html.Thead([
+                    html.Tr([
+                        html.Th('Commodity'),
+                        html.Th('Price per to'),
+                        html.Th('Amount in to'),
+                        html.Th('Date of fullfillment'),
+                        html.Th('Date of contract')                    
+                        ])
+                    ]),
+                html.Tbody([
+                    html.Tr([
+                        html.Td(dcc.Dropdown(['wheat', 'barley', 'corn', 'rapeseed'], 'wheat', id='commodity-dropdown')),
+                        html.Td([dcc.Input(id='input-price', type="number", value='80')]),
+                        html.Td([dcc.Input(id='input-amount', type="number", value='300')]),
+                        html.Td([dcc.DatePickerSingle(
+                                    id='input-date_fullfillment',
+                                    min_date_allowed=date.today(),
+                                    max_date_allowed=date.today() + timedelta(days=365*5),
+                                    
+                                    date=date.today()
+                                )]),
+                            
+                        html.Td([dcc.DatePickerSingle(
+                                    id='input-date_contract',
+                                    min_date_allowed=date.today(),
+                                    max_date_allowed=date.today() + timedelta(days=365*5),
+                                    
+                                    date=date.today() + timedelta(days=365/2)
+                                )]),
+                        ])
+                    ])     
+        
+                ]),
+            #Button to enter a contract 
+            html.Button('enter-contract', n_clicks=0, id='button-enter-contract-state'),
+            html.Div(id='output-container-button',
+                 children='Press to add contract')
+        ])
+    
+    #Menu to enter prospected harvest
+   
+    ])    
             
-            #Heading Commodity
-            html.H3('Wheat price'),
-                        
-        ])
-    elif tab == 'tab-2-example-graph':
-        return html.Div([
-            html.H3('Tab content 2'),
-        ])
+#Callbacks
+
+# Changing tab:
+
+@app.callback(Output('tabs-content', 'children'),
+             Input('commodities-tab', 'value'))
+def render_commodity(tab):
+    return tab
+            
+            
+@app.callback(Output('output-container-button', 'children'),
+            Input('button-enter-contract-state', 'n_clicks'),
+              State('commodity-dropdown', 'value'),
+              State('input-price', 'value'),
+              State('input-amount', 'value'),
+              State('input-date_fullfillment', 'date'),
+              State('input-date_contract', 'date')
+             )
+def add_contract(n_clicks, commodity, price, amount, date_fullfillment, date_contract):
+    output_text = f'The contract has been saved, Button clicked: {n_clicks}'
+    
+    contract = pd.DataFrame({'commodity': commodity, 'price': price, 'amount': amount, 'date_fullfillment':date_fullfillment, 'date_contract':date_contract}, index=[0])
+    contract.to_csv('Data/contract.csv')
+    return output_text
     
 
 
