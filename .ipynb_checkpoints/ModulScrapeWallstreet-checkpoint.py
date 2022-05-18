a9697f4108f0b36303b7ac7aa88328ad1a4ac1b0
Christoph Rendel <c.rendel@posteo.de>
2022-05-18 16:13:04 +0200

    sdf

--- .ipynb_checkpoints/ModulScrapeWallstreet-checkpoint.py
+++ .ipynb_checkpoints/ModulScrapeWallstreet-checkpoint.py
@@ -10,53 +10,81 @@ import sqlite3
 
 
 def scrape_futureprice():
-    url = 'https://www.wallstreet-online.de/rohstoffe/hu0002045586-weizen-1-tonne-1000-kg-europe-preis'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html')
+    '''Scrapes wallstreet-online for current day future prices'''
+    try: 
+        url = 'https://www.wallstreet-online.de/rohstoffe/hu0002045586-weizen-1-tonne-1000-kg-europe-preis'
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, features="lxml")
+        print(f'Server respsonse: {page}')
+    except:
+        print('Could not retrieve data from Wallstreet online')
+    
+    
     ## Table to Dateframe
     future_price_today = pd.DataFrame(columns=['Kontrakt', 'letzer_kurs', 'absolut', 'perf_perc', 'Fälligkeit', 'Vergleich', 'date_scraped'])
-    for i in soup.select('div:nth-child(11) > table > tbody >tr'):
-        row = []
-        for j in i.select('td'):
-            row.append(j.get_text())
-        row.append(date.today() - timedelta(days=1))
-        ser = pd.Series(row, index=['Kontrakt', 'letzer_kurs', 'absolut', 'perf_perc', 'Fälligkeit', 'Vergleich', 'date_scraped'])
-        future_price_today = future_price_today.append(ser, ignore_index=True)
-       
-    # Split currency
-    future_price_today['currency']=[re.split('(\d+,\d+)', string)[2] for string in future_price_today['absolut']]
-    future_price_today['absolut']=[re.split('(\d+,\d+)', string)[1] for string in future_price_today['absolut']]
-    future_price_today['letzer_kurs']=[re.split('(\d+,\d+)', string)[1] for string in future_price_today['letzer_kurs']]
-    future_price_today.drop(['Vergleich'], inplace=True, axis=1)
-    future_price_today.columns =['kontrakt', 'price', 'absolut_inc', 'perc_inc', 'date_fullfillment', 'date_price', 'currency']
-        
-    future_price_today['date_fullfillment'] = pd.to_datetime(future_price_today['date_fullfillment'], format='%d.%m.%Y')
-    future_price_today['date_price'] = pd.to_datetime(future_price_today['date_price'], format='%Y-%m-%d')
-      
-    
-    future_price_today['price'] = [string.replace(',','.') for string in future_price_today['price']]
-    future_price_today['price'] = pd.to_numeric(future_price_today['price'])
-    future_price_today['commodity_id'] = 2
+    try:
+        for i in soup.select('div:nth-child(11) > table > tbody >tr'): #extrakt tablerows
+            row = []
+            for j in i.select('td'): # extract table data of rows
+                row.append(j.get_text())
+            row.append(date.today() - timedelta(days=1))
+            ser = pd.Series(row, index=['Kontrakt', 'letzer_kurs', 'absolut', 'perf_perc', 'Fälligkeit', 'Vergleich', 'date_scraped'])
+            
+            future_price_today = pd.concat([future_price_today, pd.DataFrame(ser).transpose()], ignore_index=True)
+        print("Managed to load data into dataframe 'future_price_today'")
+        #print(future_price_today)
+    except:
+        print("Error loading data into dataframe 'future_price_today'")
+    try:
+        # Data cleaning
+        future_price_today['currency']=[re.split('(\d+,\d+)', string)[2] for string in future_price_today['absolut']] #splits currency from price
+        future_price_today['absolut']=[re.split('(\d+,\d+)', string)[1] for string in future_price_today['absolut']]
+        future_price_today['letzer_kurs']=[re.split('(\d+,\d+)', string)[1] for string in future_price_today['letzer_kurs']]
+        future_price_today.drop(['Vergleich'], inplace=True, axis=1)
+        future_price_today.columns =['kontrakt', 'price', 'absolut_inc', 'perc_inc', 'date_fullfillment', 'date_price', 'currency'] # rename column names
+
+        #change datatype from string to date
+        future_price_today['date_fullfillment'] = pd.to_datetime(future_price_today['date_fullfillment'], format='%d.%m.%Y') 
+        future_price_today['date_price'] = pd.to_datetime(future_price_today['date_price'], format='%Y-%m-%d')
+
+        #change coma seperation of decimals to point
+        future_price_today['price'] = [string.replace(',','.') for string in future_price_today['price']]
+        future_price_today['price'] = pd.to_numeric(future_price_today['price'])
+        future_price_today['commodity_id'] = 2 # commodity-id 2 = wheat
+        print(f"Data loaded from wallstreet online:/n {future_price_today}")
+    except: 
+        print("Data cleaning was not succesful")
     return future_price_today
 
 
 
 def future_price_to_sql(future_price_today):
-    con = sqlite3.connect('contrcalc.db')
-    cur = con.cursor()
-    for i in range(0, len(future_price_today)):
-        cur.execute("""
-        INSERT INTO price_table (
-        commodity_id, date_fullfillment, date_price, price, currency) 
-        VALUES (?,?,?,?,?)   
-        """,
-                   (future_price_today.iloc[i, 7],\
-                    future_price_today.iloc[i, 4],\
-                    future_price_today.iloc[i, 5],\
-                    future_price_today.iloc[i, 1],\
-                    future_price_today.iloc[i, 6])
-                   )
-    con.commit()
+    ''' Adds future_price today to the sql database price_table'''
+    print('Attempt to parse future_price_today to sql database')
+    #print(future_price_today.columns)
+    tosql_df = future_price_today[['commodity_id', 'date_fullfillment', 'date_price', 'price', 'currency']]
+    #print(tosql_df)
+    try:
+        con = sqlite3.connect('contrcalc.db') 
+        tosql_df.to_sql('price_table', con, if_exists='append', index=False)
+        print('Added price data to price table.')
+    except:
+        print('Error, failed to add data to price_table')
     return
+
+
+#Simulate random wheat price in the future
+def extrapolate_data(future_df):
+    '''Produce fake future price data'''
+    for i in range(1,9): #Takes the first 8 futures and add random variation for the next 40 days in the future
+        for day in range(40):
+            ts = future_df.iloc[i]["date_price"] + timedelta(days=day) #Adds one day to date of the price
+            price = future_df.iloc[i]['price'] + randint(-10, 10) #Adds randomnes to the price
+            series = future_df.iloc[i].copy()
+            series["date_price"] = ts    
+            series["price"] = price
+            future_df = pd.concat([future_df, pd.DataFrame(series).transpose()], axis=0)
+    return future_df
+
 
     
--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import pandas as pd
 from datetime import date, timedelta, datetime
 import sqlite3
 import plotly.graph_objects as go
+import plotly.express as px
+from ModulScrapeWallstreet import scrape_futureprice, future_price_to_sql, extrapolate_data
 
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+colorscales = px.colors.named_colorscales()
 
 #load table with price data
 
@@ -38,6 +41,8 @@ app.layout = html.Div([
     html.H1('Contract Simulator'),
     #Load Button
     html.Button('Load Data', n_clicks=0, id='button-load_data'),
+    html.Button('Load todays prices', n_clicks=0, id='button-scrape_data'),
+    html.Div(id='output-scraped_data'),
         
     html.Div([
     
@@ -63,20 +68,15 @@ app.layout = html.Div([
     
 #        dcc.RangeSlider((date.today() - timedelta(days=30)).date(), date.today() + timedelta(days=180), #7, count=1, value=[datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')], #id='rangeslider_time'),
     
-    #DatePickerRange to adapt RangSlider
-        dcc.DatePickerRange(
-        id='date-picker-range',
-        start_date=date.today(),
-        end_date_placeholder_text=date.today()
-        ),
+#    #DatePickerRange to adapt RangSlider
+#        dcc.DatePickerRange(
+#        id='date-picker-range',
+#        start_date=date.today(),
+#        end_date_placeholder_text=date.today()
+#        ),
     
     
-    #Table showing contracts
- 
-    dash_table.DataTable(
-    contracts_df.to_dict('records'),
-    [{"name": i, "id": i} for i in contracts_df.columns]
-    ),
+   
         
         
         
@@ -96,38 +96,85 @@ app.layout = html.Div([
                         ])
                     ]),
                 html.Tbody([
+                    html.Tr([html.H4('Add a contract')]),
                     html.Tr([
                         html.Td([dcc.Dropdown([com for com in commodities_df['name']], commodities_df['name'][2], id='commodity-dropdown')]),
-                        html.Td([dcc.Input(id='input-price', type="number", value=250)]), #Try to set the value to the current price of the future with the respective fullfillment date
+                        html.Td([dcc.Input(id='input-price', type="number", value='')]), 
                         html.Td([dcc.Input(id='input-amount', type="number", value='300')]), 
                         html.Td([dcc.DatePickerSingle(
                                     id='input-date_fullfillment',
-                                    min_date_allowed=date.today(),
-                                    max_date_allowed=date.today() + timedelta(days=365*5),
+                                    #min_date_allowed=date.today(),
+                                    #max_date_allowed=date.today() + timedelta(days=365*5),
                                     
                                     date=date.today() + timedelta(days=365/2)
                                 )]),
                             
                         html.Td([dcc.DatePickerSingle(
                                     id='input-date_contract',
-                                    min_date_allowed=date.today(),
-                                    max_date_allowed=date.today() + timedelta(days=365*5),
+                                    #min_date_allowed=date.today(),
+                                    #max_date_allowed=date.today() + timedelta(days=365*5),
                                     
                                     date=date.today() 
-                                )])
-                        
-                    ])     
-        
-                ]),
-            #Button to enter a contract 
-            html.Button('enter-contract', n_clicks=0, id='button-enter-contract-state'),
+                                )]),
+                        html.Td([html.Button('enter contract', n_clicks=0, id='button-enter-contract-state'),
             html.Div(id='output-container-button',
                  children='Press to add contract')
+                            
+                        ])
+                        
+                    ]),
+                    
+#Menu to delete a contract
+                    html.Tr([html.H4('Delete a contract')]),
+                    html.Tr([
+                        
+                        html.Td([dcc.Dropdown([com for com in commodities_df['name']], commodities_df['name'][2], id='commodity-dropdown-delete')]),
+                        html.Td([dcc.Input(id='input-price-delete', type="number", value='')]), 
+                        html.Td([dcc.Input(id='input-amount-delete', type="number", value='300')]), 
+                        html.Td([dcc.DatePickerSingle(
+                                    id='input-date_fullfillment-delete',
+                                    #min_date_allowed=date.today(),
+                                    #max_date_allowed=date.today() + timedelta(days=365*5),
+                                    
+                                    date=date.today() + timedelta(days=365/2)
+                                )]),
+                            
+                        html.Td([dcc.DatePickerSingle(
+                                    id='input-date_contract-delete',
+                                    #min_date_allowed=date.today(),
+                                    #max_date_allowed=date.today() + timedelta(days=365*5),
+                                    
+                                    date=date.today() 
+                                )]),
+                        html.Td([html.Button('delete contract', n_clicks=0, id='button-delete-contract-state')
+            
+                            
+                        ])
+                        
+                    ]),
+        html.Div(id='delete-button', children='Press to select contracts to delete')
+                ])
+            #Button to enter a contract 
+            
             ]),
     
+            
+             #Table showing contracts
+ 
+    dash_table.DataTable(
+    contracts_df.to_dict('records'),
+    [{"name": i, "id": i} for i in contracts_df.columns]
+    ),
+            
+            
+            
     #Menu to enter prospected harvest
         dash_table.DataTable(
         commodities_df.to_dict('records'),
+        [{"name": i, "id": i} for i in commodities_df.columns]
+        ),
+             dash_table.DataTable(
+        price_df.to_dict('records'),
         [{"name": i, "id": i} for i in commodities_df.columns]
         )
    
@@ -150,13 +197,13 @@ app.layout = html.Div([
 
 #Callbacks
 
-#load data
+#Button load data
 
 @app.callback(Output('main_div', 'children'),
     Input('button-load_data', 'n_clicks'))
 def load_data(n_clicks):
     global commodities_df, contracts_df, price_df
-     
+    print('Attempt to load commodites_df, contracts_df and price_df from sql database') 
     try:
         con = sqlite3.connect('contrcalc.db')
         commodities_df = pd.read_sql("Select * from commodities", con, index_col='commodity_id')
@@ -166,6 +213,24 @@ def load_data(n_clicks):
     except:
         print("Error: couldn't reload data from database")
     return commodities_df, contracts_df, price_df
+
+#Button load todays prices:
+
+@app.callback(Output('output-scraped_data', 'children'),
+              Input('button-scrape_data', 'n_clicks'))
+def scrape_data(n_clicks):
+    print("Attempt to scrape future price data.")
+    try:
+        future_price_today = scrape_futureprice()
+        print('Scraped Wallstreet sucessfuly')
+    except:
+        print("Failed to scrape Wallstreet")
+    try:
+        tosql_df = future_price_to_sql(future_price_today)
+        print(f'Successfully parsed data to database price_table. Parsed data: {tosql_df}')
+    except: 
+        print('Error: Could not parse data to database price_table')
+    return #future_price_today
 
 
 # Changing tab:
@@ -177,17 +242,71 @@ def load_data(n_clicks):
 
 
 # Changing date range:
-@app.callback(Output('rangeslider_time', 'value'[1]),
-              Input('date-picker-range', 'start_date'),
-             Input('date-picker-range', 'end_date'))
-def date_to_slider(start_date, end_date):
-    #print(start_date, end_date)
-    #start_data =int(start_date)
-    #end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    return start_date, end_date
+#@app.callback(Output('rangeslider_time', 'value'[1]),
+#              Input('date-picker-range', 'start_date'),
+#             Input('date-picker-range', 'end_date'))
+#def date_to_slider(start_date, end_date):
+#    #print(start_date, end_date)
+#    #start_data =int(start_date)
+#    #end_date = datetime.strptime(end_date, '%Y-%m-%d')
+#    return start_date, end_date
 
 
-#Adding Contract
+#Delete Contract
+@app.callback(Output('delete-button', 'children'),
+            Input('button-delete-contract-state', 'n_clicks'),
+              State('commodity-dropdown-delete', 'value'),
+              State('input-price-delete', 'value'),
+              State('input-amount-delete', 'value'),
+              State('input-date_fullfillment-delete', 'date'),
+              State('input-date_contract-delete', 'date')
+             )           
+def delete_contract(n_clicks, commodity, price, amount, date_fullfillment, date_contract):
+    global contracts_df
+    contract_commodity_id = commodities_df[commodities_df['name']==commodity].index  #The id of the commodity that is the subject of the contract  
+    contract = pd.DataFrame({'commodity_id': contract_commodity_id, 'price_per_to': price, 'amount_to': amount, 'date_fullfillment':date_fullfillment, 'date_closure':date_contract}, index=[0])
+    contracts_df = pd.concat([contracts_df, contract], axis = 0)
+    
+    print(f'Attempt to delete contract: {contract_commodity_id, price, amount, date_fullfillment, date_contract}')
+    querry = """
+    SELECT * FROM contracts
+        WHERE (price_per_to = :price)"""
+    #sends the new contract to sql:
+    output = 'Select features of contract to delete.'
+    try:
+        if price:
+            print(f'Connecting to db to collect contract')
+            con = sqlite3.connect('contrcalc.db')
+            to_delete = pd.read_sql(querry, con, params = {'price':price})
+            output = f'Do you want to delete this contract? {to_delete}'
+            
+            #print(f'Contract saved: n_clicks:{n_clicks}, commodity:{commodity}, price:{price}, amount:{amount}, date_fullfillment:{date_fullfillment}, date_contract:{date_contract}')
+        else:
+            print('No contract selected, some input is not clear.')
+            pass
+    except:
+        print('Failed to select a contract from database')
+    
+    try:
+        print('Deleting contracts')
+        con = sqlite3.connect('contrcalc.db')
+        cur = con.cursor()
+        cur.execute(""" DELETE FROM contracts
+                                WHERE ( 
+                                price_per_to = :price
+                                AND amount_to = :amount
+                                AND date_fullfillment = :date_ff
+                                AND date_closure = :date_c
+                                )
+            """, {"contract_commodity_id": contract_commodity_id, 'price': price, 'amount': amount, 'date_ff': date_fullfillment, 'date_c':date_contract})
+        con.close()
+        output += 'Contract deleted'
+    except: 
+        print('Something went wrong while trying to delte contract')
+    return output
+
+
+#Add Contract
            
 @app.callback(Output('output-container-button', 'children'),
             Input('button-enter-contract-state', 'n_clicks'),
@@ -199,55 +318,77 @@ def date_to_slider(start_date, end_date):
              )
 def add_contract(n_clicks, commodity, price, amount, date_fullfillment, date_contract):
     global contracts_df
+    print('Attempt to add contract')
     contract_commodity_id = commodities_df[commodities_df['name']==commodity].index  #The id of the commodity that the is the subject of the contract  
     contract = pd.DataFrame({'commodity_id': contract_commodity_id, 'price_per_to': price, 'amount_to': amount, 'date_fullfillment':date_fullfillment, 'date_closure':date_contract}, index=[0])
     contracts_df = pd.concat([contracts_df, contract], axis = 0)
     
     #sends the new contract to sql:
     try:
-        con = sqlite3.connect('contrcalc.db')
-        contract.to_sql('contracts', con, if_exists='append', index=False)
-        print(f'Contract saved: n_clicks:{n_clicks}, commodity:{commodity}, price:{price}, amount:{amount}, date_fullfillment:{date_fullfillment}, date_contract:{date_contract}')
+        if price:
+            con = sqlite3.connect('contrcalc.db')
+            contract.to_sql('contracts', con, if_exists='append', index=False)
+            print(f'Contract saved: n_clicks:{n_clicks}, commodity:{commodity}, price:{price}, amount:{amount}, date_fullfillment:{date_fullfillment}, date_contract:{date_contract}')
+        else:
+            print('No contract added because, price is empty.')
+            pass
     except:
         print('Failed to send the dataframe to the database')
     
     return
 
+
+
+
+
+
 #Display only price data that was selected in Dropdown
 @app.callback(Output('price_dev', 'figure'),
              Input('date_fulllfillment-dropdown', 'value'))
 def display_price(dates_ff):
-    #print(dates_ff)
+    print('Adding Data to Figure')
     #print(price_df['price'])
     print(price_df['date_fullfillment'])
     fig = go.Figure()
        
     # Plot the prices of the selected Futures on the y axis and the date of these prices on the y axis   
     dff = dates_ff  #all available closing dates of Futures
-    for future in enumerate(dff):
-        print(future)
-        fullfill_series = price_df['date_fullfillment'] #
-        selected_futures = fullfill_series.isin(future) # series of the values that where selected in 'date_fulllfillment-dropdown' (fullfillment dates of Futures)
-        day_of_price_of_selected_futures = price_df[selected_futures]['date_price']
-        price_of_selected_futures= price_df['price']
-        fig.add_trace(go.Scatter(x=day_of_price_of_selected_futures, y=price_of_selected_futures))
-    
+    print(f'Available closing dates: {dff}')
+    for index, future in enumerate(dff):
+        print(f'Future to add to figure: {index, future}')
+        try:
+            fullfill_series = price_df['date_fullfillment'] #
+            print(f'All fullfillment dates in price_df: {fullfill_series}')
+            #print(fullfill_series.isin(future))
+            #selected_futures = fullfill_series[future] # series of the values that where selected in 'date_fulllfillment-dropdown' (fullfillment dates of Futures)
+            #print(f'selected futures: {selected_futures}')
+            day_of_price_of_selected_futures = price_df[price_df['date_fullfillment']==future]['date_price']
+            price_of_selected_futures= price_df['price']
+            fig.add_trace(go.Scatter(x=day_of_price_of_selected_futures, y=price_of_selected_futures, name=future))
+            fig.update_traces(marker=dict(colorscale='Agsunset'))
+            print('Added future to figure')
+        except:
+            print('Could not add future to figure')
     
     #Plot the prices per to of closed contracts on the y axis and the dates of closure on the x axis as points
     price_per_to_of_closed_contracts = contracts_df['price_per_to']
     dates_of_closed_contracts = contracts_df['date_closure']
-    for index_of_closed_contract, closed_contract in enumerate(dates_of_closed_contracts):
-        print(index_of_closed_contract, closed_contract)
-        date_of_closed_contract = dates_of_closed_contracts[index_of_closed_contract]
-        price_of_closed_contract = price_per_to_of_closed_contracts[index_of_closed_contract]
-        print(f'date_of_closed_contract:{date_of_closed_contract}, price_of_closed_contract: {price_of_closed_contract}.')
-        fig.add_trace(go.Scatter(x=[date_of_closed_contract], y=[price_of_closed_contract]))
+    fullfillment_dates_of_contracts = contracts_df['date_fullfillment']
+    print(fullfillment_dates_of_contracts)
+    try:    
+        
+        fig.add_trace(go.Scatter(name="date/price closed", x=dates_of_closed_contracts, y=price_per_to_of_closed_contracts, mode='markers'))
+        #fig.update_trace(marker=dict(colorscale='viridis'))
+           
+        print(f'Added to Scatterplot: date_of_closed_contract:{dates_of_closed_contracts}, price_of_closed_contract: {price_per_to_of_closed_contracts}.')
+    except:
+        print('Failed to add dates_of_closed_contract and price_per_to_of_closed_contractst to figure')
         
         
         
         
         
-    fig.update_traces(marker=dict(size=12,
+    fig.update_traces(marker=dict(size=10,
                               line=dict(width=2,
                                         color='DarkSlateGrey')),
                   selector=dict(mode='markers'))
